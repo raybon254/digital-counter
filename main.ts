a2fc96151ace182114255de5faac5d97d4e5dc10
raybon254 <114833250+raybon254@users.noreply.github.com>
2026-02-01 11:09:33 +0300

    MICROBIT: Digital clock changing counter by +1

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(count)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(count)
+    count = 0
+})
+input.onButtonPressed(Button.B, function () {
+    count += 1
+    basic.showNumber(count)
+})
+let count = 0
+basic.showNumber(0)
+count = 0
